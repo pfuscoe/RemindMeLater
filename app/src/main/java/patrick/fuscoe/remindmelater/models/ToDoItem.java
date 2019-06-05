e4a35032cc0fefbef7dea6d350279e506d5e998c
Patrick Fuscoe <pfuscoe@usc.edu>
2019-06-05 11:51:47 -0700

    Added ToDoItem class

---
+++ app/src/main/java/patrick/fuscoe/remindmelater/models/ToDoItem.java
@@ -0,0 +1,25 @@
+package patrick.fuscoe.remindmelater.models;
+
+public class ToDoItem {
+
+    private String itemName;
+    private int priority;
+
+
+    public ToDoItem(String itemName, int priority)
+    {
+        this.itemName = itemName;
+        this.priority = priority;
+    }
+
+    public String getItemName()
+    {
+        return this.itemName;
+    }
+
+    public int getPriority()
+    {
+        return this.priority;
+    }
+
+}
