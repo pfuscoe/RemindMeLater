00e8891af07ef2e1a01bc1b3a837b041f2f48ca2
Patrick Fuscoe <patrick.fuscoe@gmail.com>
2019-06-24 22:12:28 -0700

    Setting up to do item list recycler

--- app/src/main/java/patrick/fuscoe/remindmelater/ToDoItemListActivity.java
+++ app/src/main/java/patrick/fuscoe/remindmelater/ToDoItemListActivity.java
@@ -4,10 +4,51 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import patrick.fuscoe.remindmelater.models.ToDoItem;
+
 public class ToDoItemListActivity extends AppCompatActivity {
+
+    public static final String TAG = "patrick.fuscoe.remindmelater.ToDoItemListActivity";
+
+
+    private RecyclerView toDoItemListRecyclerView;
+    private RecyclerView.Adapter toDoItemListAdapter;
+    private RecyclerView.LayoutManager toDoItemListLayoutManager;
+
+    private List<ToDoItem> toDoItemList;
+
+
+    private ToDoItemClickListener toDoItemClickListener = new ToDoItemClickListener() {
+        @Override
+        public void toDoItemClicked(View v, int position) {
+
+            Log.d(TAG, ": To Do Item at pos: " + position + " clicked");
+
+            // TODO: Setup ToDoItem click action
+
+            /*
+            String viewItemURLString = similarItemsList.get(position).getViewItemURLString();
+
+            // might need try statement
+            Uri page = Uri.parse(viewItemURLString);
+            Intent intent = new Intent(Intent.ACTION_VIEW, page);
+            startActivity(intent);
+            */
+        }
+    };
+
+    public interface ToDoItemClickListener {
+        void toDoItemClicked(View v, int position);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +56,18 @@ public class ToDoItemListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_to_do_item_list);
         Toolbar toolbar = findViewById(R.id.toolbar_to_do_item_list);
         setSupportActionBar(toolbar);
+
+        toDoItemList = new ArrayList<>();
+
+        // Setup RecyclerView
+        toDoItemListRecyclerView = findViewById(R.id.view_to_do_item_list_recycler);
+        toDoItemListRecyclerView.setHasFixedSize(true);
+
+        toDoItemListLayoutManager = new LinearLayoutManager(this);
+        toDoItemListRecyclerView.setLayoutManager(toDoItemListLayoutManager);
+
+
+
 
         /*
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -30,5 +83,7 @@ public class ToDoItemListActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("placeholder");
     }
+
+
 
 }
