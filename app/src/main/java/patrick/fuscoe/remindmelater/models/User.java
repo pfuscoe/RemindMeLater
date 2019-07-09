5a0a5e59caa213cca565f18c1e2936e32ae0c8ab
Patrick Fuscoe <patrick.fuscoe@gmail.com>
2019-07-09 13:54:11 -0700

    Added User data model class

--- app/src/main/java/patrick/fuscoe/remindmelater/models/User.java
+++ app/src/main/java/patrick/fuscoe/remindmelater/models/User.java
@@ -1,5 +1,6 @@
 package patrick.fuscoe.remindmelater.models;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class User {
@@ -28,9 +29,35 @@ public class User {
     }
 
 
-    public addSubscription(String id)
+    public void addSubscription(String groupId)
     {
-        //subscriptions =
+        ArrayList<String> tempList = new ArrayList<String>(Arrays.asList(subscriptions));
+        tempList.add(groupId);
+        subscriptions = tempList.toArray(subscriptions);
     }
 
+    /** Getters and Setters **/
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String[] getSubscriptions() {
+        return subscriptions;
+    }
+
+    public void setSubscriptions(String[] subscriptions) {
+        this.subscriptions = subscriptions;
+    }
 }
