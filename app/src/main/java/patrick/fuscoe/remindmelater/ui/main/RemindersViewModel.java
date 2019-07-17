f2d99f2e1a3bfe627fda0cbfe4d5178c37662d85
Patrick Fuscoe <pfuscoe@usc.edu>
2019-07-17 16:25:33 -0700

    Added Reminders ViewModel

---
+++ app/src/main/java/patrick/fuscoe/remindmelater/ui/main/RemindersViewModel.java
@@ -0,0 +1,32 @@
+package patrick.fuscoe.remindmelater.ui.main;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import patrick.fuscoe.remindmelater.FirebaseQueryLiveData;
+
+public class RemindersViewModel extends ViewModel {
+
+    private static final FirebaseAuth auth = FirebaseAuth.getInstance();
+    private static final String userId = auth.getCurrentUser().getUid();
+
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final CollectionReference reminders = db.collection("reminders");
+
+    private final Query remindersQuery = reminders.whereArrayContains("userId", userId);
+
+    private final FirebaseQueryLiveData liveData = new FirebaseQueryLiveData(remindersQuery);
+
+    @NonNull
+    public LiveData<QuerySnapshot> getQuerySnapshotLiveData() {
+        return liveData;
+    }
+
+}
