c5b9bfdad461aac03ec5b3be8fd95a26b6883f95
Patrick Fuscoe <patrick.fuscoe@gmail.com>
2019-07-30 16:06:15 -0700

    Working on category icon select adapter

---
+++ app/src/main/java/patrick/fuscoe/remindmelater/ui/dialog/AddReminderCategoryDialogFragment.java
@@ -0,0 +1,67 @@
+package patrick.fuscoe.remindmelater.ui.dialog;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import patrick.fuscoe.remindmelater.R;
+
+public class AddReminderCategoryDialogFragment extends DialogFragment {
+
+    public static final String TAG = "patrick.fuscoe.remindmelater.AddReminderCategoryDialogFragment";
+
+    public interface AddReminderCategoryDialogListener {
+        void onDialogPositiveClick(DialogFragment dialog);
+        void onDialogNegativeClick(DialogFragment dialog);
+    }
+
+    AddReminderCategoryDialogListener listener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            listener = (AddReminderCategoryDialogListener) getTargetFragment();
+        } catch (ClassCastException e) {
+            throw new ClassCastException("Host must implement AddReminderCategoryDialogListener");
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View v = inflater.inflate(R.layout.dialog_category_edit, null);
+
+        //Bundle bundle = getArguments();
+
+        EditText viewCategoryName = v.findViewById(R.id.dialog_category_edit_name);
+
+        // TODO: Setup recycler and adapter
+
+        builder.setView(v)
+                .setTitle(R.string.dialog_add_reminder_category_title)
+                .setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        listener.onDialogPositiveClick(AddReminderCategoryDialogFragment.this);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        listener.onDialogNegativeClick(AddReminderCategoryDialogFragment.this);
+                    }
+                });
+        return builder.create();
+    }
+
+}
---
+++ app/src/main/java/patrick/fuscoe/remindmelater/ui/dialog/CategoryIconSelectAdapter.java
@@ -0,0 +1,15 @@
+package patrick.fuscoe.remindmelater.ui.dialog;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+public class CategoryIconSelectAdapter extends RecyclerView.Adapter<CategoryIconSelectAdapter.IconViewHolder> {
+
+    private List<Integer> categoryIconList;
+    private Context context;
+
+    
+
+}
