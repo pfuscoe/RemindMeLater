9cb57c8e72aabe26815da6d17b6180df823ea48f
Patrick Fuscoe <patrick.fuscoe@gmail.com>
2019-07-30 16:37:21 -0700

    Working on category icon select adapter 2

--- app/src/main/java/patrick/fuscoe/remindmelater/ui/dialog/CategoryIconSelectAdapter.java
+++ app/src/main/java/patrick/fuscoe/remindmelater/ui/dialog/CategoryIconSelectAdapter.java
@@ -1,7 +1,9 @@
 package patrick.fuscoe.remindmelater.ui.dialog;
 
 import android.content.Context;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.List;
 
@@ -9,7 +11,13 @@ public class CategoryIconSelectAdapter extends RecyclerView.Adapter<CategoryIcon
 
     private List<Integer> categoryIconList;
     private Context context;
+    private int selectedIcon;
 
-    
+    public static class IconViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        ConstraintLayout viewGridItemLayout;
+        
+
+    }
 
 }
