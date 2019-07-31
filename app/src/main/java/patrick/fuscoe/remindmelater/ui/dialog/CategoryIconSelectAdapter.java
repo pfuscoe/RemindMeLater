d74114090e5ae12e382bbb65e888d5a7955bea7b
Patrick Fuscoe <patrick.fuscoe@gmail.com>
2019-07-30 17:11:20 -0700

    Working on category icon select adapter 3

--- app/src/main/java/patrick/fuscoe/remindmelater/ui/dialog/CategoryIconSelectAdapter.java
+++ app/src/main/java/patrick/fuscoe/remindmelater/ui/dialog/CategoryIconSelectAdapter.java
@@ -4,20 +4,40 @@ import android.content.Context;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 
 import java.util.List;
+
+import patrick.fuscoe.remindmelater.R;
 
 public class CategoryIconSelectAdapter extends RecyclerView.Adapter<CategoryIconSelectAdapter.IconViewHolder> {
 
     private List<Integer> categoryIconList;
     private Context context;
     private int selectedIcon;
+    private int selectedIconPos;
 
     public static class IconViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        ConstraintLayout viewGridItemLayout;
-        
+        ConstraintLayout viewCategoryItemLayout;
+        ImageView viewCategoryIcon;
+        ImageView viewCategoryIconCheckbox;
 
+        IconViewHolder(View v)
+        {
+            super(v);
+
+            v.setOnClickListener(this);
+
+            viewCategoryItemLayout = v.findViewById(R.id.grid_item_icon_select_layout);
+            viewCategoryIcon = v.findViewById(R.id.grid_item_icon);
+            viewCategoryIconCheckbox = v.findViewById(R.id.grid_item_checkbox);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
     }
 
 }
