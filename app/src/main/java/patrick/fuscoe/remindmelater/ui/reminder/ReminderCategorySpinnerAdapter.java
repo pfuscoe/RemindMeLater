e1a2baf403387aa45288884ff7af0aa12578c72a
Patrick Fuscoe <pfuscoe@usc.edu>
2019-08-06 18:08:20 -0700

    Working on custom spinner category select adapter 2

--- app/src/main/java/patrick/fuscoe/remindmelater/ui/reminder/ReminderCategorySpinnerAdapter.java
+++ app/src/main/java/patrick/fuscoe/remindmelater/ui/reminder/ReminderCategorySpinnerAdapter.java
@@ -1,9 +1,28 @@
 package patrick.fuscoe.remindmelater.ui.reminder;
 
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
 public class ReminderCategorySpinnerAdapter extends BaseAdapter {
 
-    
+    @Override
+    public int getCount() {
+        return 0;
+    }
 
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
 }
