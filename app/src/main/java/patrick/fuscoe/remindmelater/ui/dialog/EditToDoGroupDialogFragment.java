9c3c9c921400dca3fd64248bf8f4a6bf4aed4e04
Patrick Fuscoe <pfuscoe@usc.edu>
2019-08-16 16:55:54 -0700

    Working on category icons for Edit to do group 2

--- app/src/main/java/patrick/fuscoe/remindmelater/ui/dialog/EditToDoGroupDialogFragment.java
+++ app/src/main/java/patrick/fuscoe/remindmelater/ui/dialog/EditToDoGroupDialogFragment.java
@@ -7,16 +7,65 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.List;
+
 import patrick.fuscoe.remindmelater.R;
+import patrick.fuscoe.remindmelater.models.CategoryIconSet;
 import patrick.fuscoe.remindmelater.ui.main.ToDoFragment;
 
 public class EditToDoGroupDialogFragment extends DialogFragment {
 
     public static final String TAG = "patrick.fuscoe.remindmelater.EditToDoGroupDialogFragment";
+
+    private List<Integer> categoryIconList;
+    private List<Boolean> categoryIconListIsChecked;
+    private int selectedIcon;
+    private int selectedIconPos;
+
+    private RecyclerView categoryIconRecycler;
+    private RecyclerView.LayoutManager categoryIconRecyclerLayoutManager;
+    private RecyclerView.Adapter categoryIconRecyclerAdapter;
+
+    public interface CategoryIconClickListener {
+        void onIconClicked(View v, int position);
+    }
+
+    private CategoryIconClickListener categoryIconClickListener = new CategoryIconClickListener() {
+        @Override
+        public void onIconClicked(View v, int position) {
+            int oldPos = selectedIconPos;
+            selectedIconPos = position;
+
+            if (oldPos != -1)
+            {
+                categoryIconListIsChecked.set(oldPos, false);
+            }
+
+            if (oldPos == position)
+            {
+                //categoryIconListIsChecked.set(position, false);
+                selectedIcon = -1;
+                selectedIconPos = -1;
+            }
+            else
+            {
+                categoryIconListIsChecked.set(position, true);
+                selectedIcon = categoryIconList.get(position);
+
+                // notify old pos changed
+                categoryIconRecyclerAdapter.notifyItemChanged(oldPos);
+            }
+
+            // notify new pos changed
+            categoryIconRecyclerAdapter.notifyItemChanged(position);
+        }
+    };
 
     public interface EditToDoGroupDialogListener {
         public void onDialogPositiveClick(DialogFragment dialog);
@@ -41,14 +90,30 @@ public class EditToDoGroupDialogFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = requireActivity().getLayoutInflater();
-        View v = inflater.inflate(R.layout.dialog_add_to_do_group, null);
+        View v = inflater.inflate(R.layout.dialog_category_edit, null);
 
         Bundle bundle = getArguments();
         String groupTitle = bundle.getString("title");
         int groupIconId = bundle.getInt("iconId");
 
-        EditText viewGroupTitle = v.findViewById(R.id.dialog_add_to_do_group_title);
+        EditText viewGroupTitle = v.findViewById(R.id.dialog_category_edit_name);
         viewGroupTitle.setText(groupTitle);
+        selectedIcon = groupIconId;
+        //selectedIconPos = -1;
+
+        CategoryIconSet categoryIconSet = new CategoryIconSet();
+        categoryIconList = categoryIconSet.getCategoryIconList();
+        categoryIconListIsChecked = categoryIconSet.getCategoryIconListIsChecked();
+
+        categoryIconRecycler = v.findViewById(R.id.dialog_category_edit_recycler);
+        categoryIconRecycler.setHasFixedSize(true);
+
+        categoryIconRecyclerLayoutManager = new GridLayoutManager(getContext(), 5);
+        categoryIconRecycler.setLayoutManager(categoryIconRecyclerLayoutManager);
+
+        categoryIconRecyclerAdapter = new CategoryIconSelectAdapter(categoryIconList, categoryIconListIsChecked, getContext(), categoryIconClickListener);
+        categoryIconRecycler.setAdapter(categoryIconRecyclerAdapter);
+
 
         builder.setView(v)
                 .setTitle(R.string.dialog_edit_to_do_group_title)
