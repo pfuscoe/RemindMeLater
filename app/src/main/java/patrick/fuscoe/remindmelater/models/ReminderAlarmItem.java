385f94cc184f7c99545cca503f5c0a69a0d1243c
Patrick Fuscoe <patrick.fuscoe@gmail.com>
2019-08-23 12:15:58 -0700

    Setup ReminderAlarmItem

--- app/src/main/java/patrick/fuscoe/remindmelater/models/ReminderAlarmItem.java
+++ app/src/main/java/patrick/fuscoe/remindmelater/models/ReminderAlarmItem.java
@@ -1,5 +1,6 @@
 package patrick.fuscoe.remindmelater.models;
 
+import java.time.LocalDate;
 import java.util.Calendar;
 
 public class ReminderAlarmItem {
@@ -7,4 +8,45 @@ public class ReminderAlarmItem {
     private String nextOccurrence;
     private int broadcastId;
     private Calendar alarmCalendar;
+
+
+    public ReminderAlarmItem()
+    {
+
+    }
+
+    public ReminderAlarmItem(String nextOccurrence, int broadcastId, int hour, int minute)
+    {
+        this.nextOccurrence = nextOccurrence;
+        this.broadcastId = broadcastId;
+
+        buildAlarmCalendarObject(hour, minute);
+    }
+
+    private void buildAlarmCalendarObject(int hour, int minute)
+    {
+        LocalDate alarmDate = LocalDate.parse(nextOccurrence);
+        int year = alarmDate.getYear();
+        int month = alarmDate.getMonthValue();
+        int day = alarmDate.getDayOfMonth();
+
+        alarmCalendar = Calendar.getInstance();
+        alarmCalendar.setTimeInMillis(System.currentTimeMillis());
+        alarmCalendar.set(year, month - 1, day);
+        alarmCalendar.set(Calendar.HOUR_OF_DAY, hour);
+        alarmCalendar.set(Calendar.MINUTE, minute);
+    }
+
+    public String getNextOccurrence() {
+        return nextOccurrence;
+    }
+
+    public int getBroadcastId() {
+        return broadcastId;
+    }
+
+    public Calendar getAlarmCalendar() {
+        return alarmCalendar;
+    }
+
 }
