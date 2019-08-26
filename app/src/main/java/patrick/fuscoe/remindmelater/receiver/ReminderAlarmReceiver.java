a09417986238b974da319cac501c38c92a839c21
Patrick Fuscoe <patrick.fuscoe@gmail.com>
2019-08-26 12:35:10 -0700

    Working on ReminderAlarmReceiver

--- app/src/main/java/patrick/fuscoe/remindmelater/receiver/ReminderAlarmReceiver.java
+++ app/src/main/java/patrick/fuscoe/remindmelater/receiver/ReminderAlarmReceiver.java
@@ -4,10 +4,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import patrick.fuscoe.remindmelater.MainActivity;
+
 public class ReminderAlarmReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        String title = intent.getStringExtra(MainActivity.REMINDER_TITLE);
 
+        // TODO: Setup send notification service for received alarm
+        // need small icon, title
     }
 }
