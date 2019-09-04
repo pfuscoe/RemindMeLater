9cce0c9c075f152cbd57f968b112692787e4b9a5
Patrick Fuscoe <patrick.fuscoe@gmail.com>
2019-09-04 11:23:01 -0700

    Setup NotificationDoneReceiver

--- app/src/main/java/patrick/fuscoe/remindmelater/receiver/NotificationDoneReceiver.java
+++ app/src/main/java/patrick/fuscoe/remindmelater/receiver/NotificationDoneReceiver.java
@@ -3,7 +3,14 @@ package patrick.fuscoe.remindmelater.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -11,7 +18,11 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.HashMap;
 
+import patrick.fuscoe.remindmelater.R;
 import patrick.fuscoe.remindmelater.models.ReminderItem;
 
 public class NotificationDoneReceiver extends BroadcastReceiver {
@@ -45,5 +56,78 @@ public class NotificationDoneReceiver extends BroadcastReceiver {
         remindersDocId = intent.getStringExtra(ReminderAlarmReceiver.REMINDERS_DOC_ID);
         remindersDocRef = remindersCollectionRef.document(remindersDocId);
 
+        updateReminderItem();
+        saveReminderItem();
     }
+
+    private void updateReminderItem()
+    {
+        Period recurrence = reminderItem.getRecurrence();
+        int daysUntilNext = recurrence.getDays();
+
+        LocalDate nextOccurrence = LocalDate.now();
+        nextOccurrence.plusDays(daysUntilNext);
+        reminderItem.setNextOccurrence(nextOccurrence.toString());
+
+        // TODO: Add action to history
+    }
+
+    private void saveReminderItem()
+    {
+        HashMap<String, Object> reminderItemMap = new HashMap<>();
+        reminderItemMap.put("recurrence", reminderItem.getRecurrence().toString());
+        reminderItemMap.put("recurrenceNum", reminderItem.getRecurrenceNum());
+        reminderItemMap.put("recurrenceInterval", reminderItem.getRecurrenceInterval());
+        reminderItemMap.put("nextOccurrence", reminderItem.getNextOccurrence());
+        reminderItemMap.put("category", reminderItem.getCategory());
+        reminderItemMap.put("categoryIcon", reminderItem.getCategoryIcon());
+        reminderItemMap.put("description", reminderItem.getDescription());
+
+        saveReminderToSharedPreferences();
+
+        remindersDocRef.update(reminderItem.getTitle(), reminderItemMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Reminders DocumentSnapshot successfully updated!");
+                        Toast.makeText(context, "Reminder Item Updated: " + reminderItem.getTitle(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error updating reminders document", e);
+                        // TODO: handle local storage of reminder when cloud sync fails
+                    }
+                });
+    }
+
+    private void saveReminderToSharedPreferences()
+    {
+        SharedPreferences reminderAlarmStorage = context.getSharedPreferences(
+                context.getString(R.string.reminders_file_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor reminderAlarmEditor = reminderAlarmStorage.edit();
+
+        reminderAlarmEditor.putString(reminderItem.getTitle(), reminderItem.getNextOccurrence());
+        reminderAlarmEditor.apply();
+
+        SharedPreferences reminderIconIds = context.getSharedPreferences(
+                context.getString(R.string.reminder_icon_ids_file_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor reminderIconIdEditor = reminderIconIds.edit();
+
+        reminderIconIdEditor.putInt(reminderItem.getTitle(), reminderItem.getCategoryIcon());
+        reminderIconIdEditor.apply();
+
+        SharedPreferences reminderBroadcastIds = context.getSharedPreferences(
+                context.getString(R.string.reminder_broadcast_ids_file_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor reminderBroadcastIdEditor = reminderBroadcastIds.edit();
+
+        int broadcastId = (int) System.currentTimeMillis();
+        // TODO: Add check for existing id
+        reminderBroadcastIdEditor.putInt(reminderItem.getTitle(), broadcastId);
+        reminderBroadcastIdEditor.apply();
+
+        // TODO: using apply() for async saving. Check if commit() needed
+    }
+    
 }
