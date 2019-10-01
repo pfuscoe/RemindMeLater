77e6186eebc92c72e58da0e9b5179273d01ef6e2
Patrick Fuscoe <patrick.fuscoe@gmail.com>
2019-10-01 12:17:43 -0700

    Setup existing user login with email and password

--- app/src/main/java/patrick/fuscoe/remindmelater/FirebaseSignInActivity.java
+++ app/src/main/java/patrick/fuscoe/remindmelater/FirebaseSignInActivity.java
@@ -1,14 +1,23 @@
 package patrick.fuscoe.remindmelater;
 
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -21,20 +30,127 @@ public class FirebaseSignInActivity extends AppCompatActivity {
     public static final String CHECK_IF_NEW_USER = "patrick.fuscoe.remindmelater.CHECK_IF_NEW_USER";
     private static final int RC_SIGN_IN = 223;
 
+    public static final String USER_ID = "patrick.fuscoe.remindmelater.USER_ID";
+
+    private FirebaseAuth auth;
+
+    private EditText viewEmail;
+    private EditText viewPassword;
+    private TextView viewNewUserLink;
+    private TextView viewForgotPasswordLink;
+    private Button btnLogin;
+
     private boolean userMustEnterLoginInfo;
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId())
+            {
+                case R.id.view_sign_in_new_user_link:
+                    openNewUserSignUp();
+                    return;
+
+                case R.id.view_sign_in_forgot_password_link:
+                    openForgotPassword();
+                    return;
+
+                case R.id.btn_sign_in_login:
+                    loginReturningUser();
+                    return;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        auth = FirebaseAuth.getInstance();
+
         userMustEnterLoginInfo = false;
 
-        checkSignIn();
+        if (auth.getCurrentUser() != null)
+        {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        setContentView(R.layout.activity_firebase_sign_in);
+
+        viewEmail = findViewById(R.id.view_sign_in_email);
+        viewPassword = findViewById(R.id.view_sign_in_password);
+        viewNewUserLink = findViewById(R.id.view_sign_in_new_user_link);
+        viewForgotPasswordLink = findViewById(R.id.view_sign_in_forgot_password_link);
+        btnLogin = findViewById(R.id.btn_sign_in_login);
+
+        viewNewUserLink.setOnClickListener(onClickListener);
+        viewForgotPasswordLink.setOnClickListener(onClickListener);
+        btnLogin.setOnClickListener(onClickListener);
+
+        //checkSignIn();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    private void openNewUserSignUp()
+    {
+
+    }
+
+    private void openForgotPassword()
+    {
+
+    }
+
+    private void loginReturningUser()
+    {
+        String email = viewEmail.getText().toString();
+
+        if (email.equals(""))
+        {
+            Toast.makeText(FirebaseSignInActivity.this, "Please Enter Your Email Address", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String password = viewPassword.getText().toString();
+
+        if (password.equals(""))
+        {
+            Toast.makeText(FirebaseSignInActivity.this, "Please Enter Your Password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithEmail:success");
+                            //FirebaseUser user = auth.getCurrentUser();
+                            //updateUI(user);
+                            String userId = auth.getUid();
+                            Intent intent = new Intent(FirebaseSignInActivity.this, MainActivity.class);
+                            intent.putExtra(USER_ID, userId);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(FirebaseSignInActivity.this, "Authentication failed: " + task.getException().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
     }
 
     public void checkSignIn()
     {
-        FirebaseAuth auth = FirebaseAuth.getInstance();
         if (auth.getCurrentUser() != null)
         {
             // already signed in
