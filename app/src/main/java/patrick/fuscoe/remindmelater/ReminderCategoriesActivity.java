f55636c81521471a5164124a65877f84b44f6aa7
Patrick Fuscoe <patrick.fuscoe@gmail.com>
2019-10-25 11:36:55 -0700

    Setup reminder categories adapter

--- app/src/main/java/patrick/fuscoe/remindmelater/ReminderCategoriesActivity.java
+++ app/src/main/java/patrick/fuscoe/remindmelater/ReminderCategoriesActivity.java
@@ -3,8 +3,21 @@ package patrick.fuscoe.remindmelater;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ReminderCategoriesActivity extends AppCompatActivity {
+
+
+    private ReminderCategoryClickListener reminderCategoryClickListener = new ReminderCategoryClickListener() {
+        @Override
+        public void reminderCategoryClicked(View v, int position) {
+
+        }
+    };
+
+    public interface ReminderCategoryClickListener {
+        void reminderCategoryClicked(View v, int position);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/patrick/fuscoe/remindmelater/ui/main/ReminderCategoriesAdapter.java
@@ -0,0 +1,86 @@
+package patrick.fuscoe.remindmelater.ui.main;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import patrick.fuscoe.remindmelater.R;
+import patrick.fuscoe.remindmelater.ReminderCategoriesActivity;
+import patrick.fuscoe.remindmelater.models.ReminderCategory;
+
+public class ReminderCategoriesAdapter extends RecyclerView.Adapter<ReminderCategoriesAdapter.ReminderCategoryViewHolder> {
+
+    private List<ReminderCategory> reminderCategoryList;
+    private Context context;
+
+    private static ReminderCategoriesActivity.ReminderCategoryClickListener reminderCategoryClickListener;
+
+    public static class ReminderCategoryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        ConstraintLayout viewRowReminderCategoryLayout;
+        ImageView viewRowReminderCategoryIcon;
+        TextView viewRowReminderCategoryTitle;
+        ImageView viewRowReminderCategoryDeleteIcon;
+
+        ReminderCategoryViewHolder(View v)
+        {
+            super(v);
+
+            v.setOnClickListener(this);
+
+            viewRowReminderCategoryLayout = v.findViewById(R.id.view_row_reminder_category_edit_layout);
+            viewRowReminderCategoryIcon = v.findViewById(R.id.view_row_reminder_category_edit_icon);
+            viewRowReminderCategoryTitle = v.findViewById(R.id.view_row_reminder_category_edit_title);
+            viewRowReminderCategoryDeleteIcon = v.findViewById(R.id.view_row_reminder_category_delete_icon);
+        }
+
+        @Override
+        public void onClick(View v) {
+            reminderCategoryClickListener.reminderCategoryClicked(v, this.getLayoutPosition());
+        }
+    }
+
+    public ReminderCategoriesAdapter(List<ReminderCategory> reminderCategoryList, Context context,
+                                     ReminderCategoriesActivity.ReminderCategoryClickListener reminderCategoryClickListener)
+    {
+        this.reminderCategoryList = reminderCategoryList;
+        this.context = context;
+        this.reminderCategoryClickListener = reminderCategoryClickListener;
+    }
+
+    @NonNull
+    @Override
+    public ReminderCategoryViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        ConstraintLayout v = (ConstraintLayout) LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.row_reminder_category_edit, viewGroup, false);
+
+        return new ReminderCategoryViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReminderCategoryViewHolder holder, final int position) {
+
+        ReminderCategory reminderCategory = reminderCategoryList.get(position);
+
+        holder.viewRowReminderCategoryTitle.setText(reminderCategory.getCategoryName());
+
+        holder.viewRowReminderCategoryIcon.setImageResource(context.getResources().getIdentifier(
+                reminderCategory.getIconName(), "drawable", context.getPackageName()));
+        holder.viewRowReminderCategoryDeleteIcon.setImageResource(R.drawable.action_delete);
+    }
+
+    @Override
+    public int getItemCount() {
+        return reminderCategoryList.size();
+    }
+}
