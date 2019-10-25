edf70e1efa2fd6f608bfee0f29af23c8dfd03e93
Patrick Fuscoe <patrick.fuscoe@gmail.com>
2019-10-25 11:09:29 -0700

    Setup reminder categories row layout

---
+++ app/src/main/java/patrick/fuscoe/remindmelater/ReminderCategoriesActivity.java
@@ -0,0 +1,16 @@
+package patrick.fuscoe.remindmelater;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReminderCategoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminder_categories);
+
+
+    }
+}
