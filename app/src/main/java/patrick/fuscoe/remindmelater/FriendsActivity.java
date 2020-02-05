aa59f9b8d910fd4c5c4a6c609cc47e255fd09ed0
Patrick Fuscoe <patrick.fuscoe@gmail.com>
2020-02-05 10:59:44 -0800

    Added Friends Activity and layout files

---
+++ app/src/main/java/patrick/fuscoe/remindmelater/FriendsActivity.java
@@ -0,0 +1,25 @@
+package patrick.fuscoe.remindmelater;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+/**
+ * Manages UI for Friends List. Handles friend requests, To Do List sharing and requests to add
+ * reminders to friends.
+ */
+
+public class FriendsActivity extends AppCompatActivity {
+
+    public static final String TAG = "patrick.fuscoe.remindmelater.FriendsActivity";
+
+    
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+    }
+}
