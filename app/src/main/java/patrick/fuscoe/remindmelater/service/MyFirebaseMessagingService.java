c618ff27cdeda77d8493e4a37a55a49a2eff1b19
Patrick Fuscoe <patrick.fuscoe@gmail.com>
2020-02-20 10:22:11 -0800

    Updated gradle and manifest with firebase messaging service

---
+++ app/src/main/java/patrick/fuscoe/remindmelater/service/MyFirebaseMessagingService.java
@@ -0,0 +1,9 @@
+package patrick.fuscoe.remindmelater.service;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+
+
+}
