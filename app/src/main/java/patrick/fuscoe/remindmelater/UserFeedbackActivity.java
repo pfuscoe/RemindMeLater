d96bae1b901b3902754780cc93e9b6df46dbc93a
Patrick Fuscoe <patrick.fuscoe@gmail.com>
2020-04-14 22:35:26 -0700

    Setting up user feedback activity

---
+++ app/src/main/java/patrick/fuscoe/remindmelater/UserFeedbackActivity.java
@@ -0,0 +1,14 @@
+package patrick.fuscoe.remindmelater;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserFeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_feedback);
+    }
+}
