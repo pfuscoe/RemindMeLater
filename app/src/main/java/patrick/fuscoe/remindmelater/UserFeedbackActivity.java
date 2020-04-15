70fa126202cc38c15df408fc63001fb2ea2af1f0
Patrick Fuscoe <pfuscoe@usc.edu>
2020-04-15 10:49:57 -0700

    Setup user feedback activity to capture input and send from email client

--- app/src/main/java/patrick/fuscoe/remindmelater/UserFeedbackActivity.java
+++ app/src/main/java/patrick/fuscoe/remindmelater/UserFeedbackActivity.java
@@ -1,14 +1,85 @@
 package patrick.fuscoe.remindmelater;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class UserFeedbackActivity extends AppCompatActivity {
+
+    public static final String TAG = "patrick.fuscoe.remindmelater.UserFeedbackActivity";
+
+    private RadioGroup radioGroupUserFeedback;
+    private RadioButton radioReportBug;
+    private RadioButton radioOtherFeedback;
+    private EditText viewFeedbackField;
+    private Button btnSendFeedback;
+
+    private View.OnClickListener btnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId())
+            {
+                case R.id.button_user_feedback_send:
+                    sendFeedback();
+                    return;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_feedback);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        radioGroupUserFeedback = findViewById(R.id.radio_group_user_feedback);
+        radioReportBug = findViewById(R.id.radio_button_user_feedback_report_bug);
+        radioOtherFeedback = findViewById(R.id.radio_button_user_feedback_other_feedback);
+        viewFeedbackField = findViewById(R.id.view_user_feedback_field);
+        btnSendFeedback = findViewById(R.id.button_user_feedback_send);
+        
+        btnSendFeedback.setOnClickListener(btnClickListener);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Send Feedback");
+    }
+
+    // Prompt user to choose email client and auto-fill feedback
+    private void sendFeedback()
+    {
+        int radioCheckedId = radioGroupUserFeedback.getCheckedRadioButtonId();
+        String feedbackSubject = processRadioCheckedId(radioCheckedId);
+        String feedbackText = viewFeedbackField.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.putExtra(Intent.EXTRA_SUBJECT, feedbackSubject);
+        intent.putExtra(Intent.EXTRA_TEXT, feedbackText);
+        intent.setData(Uri.parse("mailto:info@patrickfuscoe.com"));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    private String processRadioCheckedId(int radioCheckedId)
+    {
+        switch (radioCheckedId)
+        {
+            case R.id.radio_button_user_feedback_report_bug:
+                return getResources().getString(R.string.user_feedback_radio_label_report_bug);
+
+            case R.id.radio_button_user_feedback_other_feedback:
+                return getResources().getString(R.string.user_feedback_radio_label_other_feedback);
+
+            default:
+                return getResources().getString(R.string.user_feedback_radio_label_other_feedback);
+        }
     }
 }
